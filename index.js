357fc3d9e05e185884b876bc83eaaf0e40179288
emShalaby <medo.dice@gmail.com>
2023-06-21 16:05:33 +0300

    css adjustments

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-function Book(name, author, pages, readStatus) {
-  this.name = name;
+function Book(title, author, pages, score, readPages) {
+  this.name = title;
   this.author = author;
-  this.pagers = pages;
-  this.readStatus = readStatus;
-  this.info = [name, author, pages, readStatus];
+  this.pages = pages;
+  let progress = readPages + "/" + pages;
+  this.info = [title, author, pages, score, progress];
 }
-let book1 = new Book("YDJS", "ME", "122 pages", "Not read yet");
-let book2 = new Book("YDJS", "ME", "122 pages", "Not read yet");
-let book3 = new Book("YDJS", "ME", "122 pages", "Not read yet");
+let book1 = new Book("ABCD", "ME", "122", 8, "50");
+let book2 = new Book("EFG", "SE", "123", 10, "60");
+let book3 = new Book("HIJK", "VE", "124", 9, "70");
 let library = document.querySelector(".library");
 let myLibrary = [book1, book2, book3];
 
 function addBookToLibrary() {}
 
 function displayBooks(book) {
+  const order = document.createElement("p");
   const div = document.createElement("div");
-  div.innerText = book1.info;
+  order.innerText = Number(myLibrary.indexOf(book, 0)) + 1;
+  div.appendChild(order);
+
   div.classList.add("book");
+  book.info.forEach((elem) => {
+    const p = document.createElement("p");
+    p.innerText = elem;
+    div.appendChild(p);
+  });
   library.appendChild(div);
 }
 
